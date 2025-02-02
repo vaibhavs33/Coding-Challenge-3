75650d189cbaa30e48773d1de26c98db63c8ee4b
Vaibhav Surapaneni <vaibhavsurapaneni1979@gmail.com>
2025-02-02 10:50:20 -0500

    Task 1 - Product Price Management

--- cc_3.js
+++ cc_3.js
@@ -1,5 +1,5 @@
 //Task 1 - Product Price Management
-let prices = [3.99, 8.00, 6.75, 11.99, 10.00];
+let prices = [3.99, 8.00, 6.75, 11.99, 10.00]; //Declaring an array called prices with 5 numerical values that represent the price of the products
 prices.push(5.50); //Adding a new price to the array
 prices.shift(); //Removing the first price from the array
 console.log("Updated prices:", prices); //Logging the updated array to the console
